86f492cf8f1e82d1603ac8a9145cd4c37a365d40
Randall Harper <randall.harper@ttu.edu>
2015-10-02 14:13:21 -0500

    testing php

--- hello_world.php
+++ hello_world.php
@@ -1,6 +1,22 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+  
+</html>
 
-echo "James Little is not the best.";
+
+
+<?php
+//add to html head
+//<meta name="viewport" content="width=device-width, initial-scale=1">
+
+echo nl2br(strrev("\r\nRandall Harper was here last.\r\n\r\n"));
+echo nl2br("Testing echo\r\nThis is a newline \r\n", false);
+echo "The time the server has is " . date("h:i:sa");
+
 
 
 ?>
+
+
+
+
