476ceaeb50a495656ad5e9c68b4c338c1e02a158
Randall Harper <randall.harper@ttu.edu>
2015-10-06 12:55:24 -0500

    made folders for organization

--- website/webapp/views/home.php
+++ website/webapp/views/home.php
@@ -3,10 +3,10 @@
     $page->showHeader();
 
 
-    echo newBlogPost("Blog Post Title", "James Little", "September 9th 1999", "http://placehold.it/900x300", "something something");
-    echo newBlogPost("Blog Post Title", "James Little", "September 9th 1999", "http://placehold.it/900x300", "something something");
-    echo newBlogPost("Blog Post Title", "James Little", "September 9th 1999", "http://placehold.it/900x300", "something something");
-    echo pager();
+    //echo newBlogPost("Blog Post Title", "James Little", "September 9th 1999", "http://placehold.it/900x300", "something something");
+    //echo newBlogPost("Blog Post Title", "James Little", "September 9th 1999", "http://placehold.it/900x300", "something something");
+    //echo newBlogPost("Blog Post Title", "James Little", "September 9th 1999", "http://placehold.it/900x300", "something something");
+    //echo pager();
 
     $page->showFooter();
 ?>
