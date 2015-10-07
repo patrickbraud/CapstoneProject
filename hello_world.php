ccf568a00e246f69b9a4ba2125eab774ffa81ff5
Randall Harper <randall.harper@ttu.edu>
2015-10-06 20:08:15 -0500

    gg

--- hello_world.php
+++ hello_world.php
@@ -6,13 +6,12 @@
 
 
 <?php
-//add to html head
-//<meta name="viewport" content="width=device-width, initial-scale=1">
+
 
 echo nl2br(strrev("\r\nRandall Harper was here last.\r\n\r\n"));
 echo nl2br("Testing echo\r\nThis is a newline \r\n", false);
 echo "The time the server has is " . date("h:i:sa");
-
+echo nl2br("Test \r\n");
 
 
 ?>
