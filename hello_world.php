0dac7c5a22dcd3d2f42b78360151c3dc45b8c644
Randall Harper <randall.harper@ttu.edu>
2015-10-06 20:36:51 -0500

    system manager
    updated system manager use case diagram

--- hello_world.php
+++ hello_world.php
@@ -10,7 +10,7 @@
 
 echo nl2br(strrev("\r\nRandall Harper was here last.\r\n\r\n"));
 echo nl2br("Testing echo\r\nThis is a newline \r\n", false);
-echo "The time the server has is " . date("h:i:sa");
+echo "The time the server has is \r\n" . date("h:i:sa");
 echo nl2br("Test \r\n");
 
 
---
+++ hello_world_testfile.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+</html>
+
+
+<?php
+
+
+echo nl2br(strrev("\r\nRandall Harper was here last.\r\n"));
+
+?>
