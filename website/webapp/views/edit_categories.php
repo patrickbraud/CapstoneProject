9459633dbff1ec0272bb74eb7daddaec13986c88
Randall Harper <randall.harper@ttu.edu>
2015-10-17 16:43:22 -0500

    randall

--- website/webapp/views/edit_categories.php
+++ website/webapp/views/edit_categories.php
@@ -1,11 +1,24 @@
 <?php
 	$page = new Page("Edit Categories Page");
 	$page->showHeader();
+?>
+<form class="form-horizontal" role="form">
+	<div class="form-group">
+	</div>
 
+	<div class="form-group">
+		<p>Categories edit page</p>
+	</div>
+</form>
 
+<div class="list-group">
+  <button type="button" class="list-group-item">Artificial Intelligence</button>
+  <button type="button" class="list-group-item">Big Data</button>
+  <button type="button" class="list-group-item">Computer Architecture</button>
+  <button type="button" class="list-group-item">Computer Engineering</button>
+  <button type="button" class="list-group-item">Computer Graphics</button>
+</div>
 
-	echo newBlogPost("Penis Post Title", "James Little is a bitch", "September 9th 1999", "http://placehold.it/900x300", "something something");
-
+<?php
 	$page->showFooter();
-
 ?>
