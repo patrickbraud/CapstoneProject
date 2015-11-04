f1eeeae344331e405d4641b44ee4e21ad96b033a
James Little <wizkid916@yahoo.com>
2015-11-04 03:00:19 -0600

    fixed the contacts page.

--- website/webapp/views/contacts.php
+++ website/webapp/views/contacts.php
@@ -6,25 +6,24 @@
 <h1>
 	<p>Administrator Contacts</p>
 </h1>
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-body">James Little</div>
+      <div class="panel-footer">Email : james.little@ttu.edu</div>
+    </div>
+  </div>
+
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-body">Patrick Braud</div>
+      <div class="panel-footer">Email : patrick.braud@ttu.edu</div>
+    </div>
+  </div>
 
 <div class="panel-group">
   <div class="panel panel-default">
     <div class="panel-body">Randall Harper</div>
     <div class="panel-footer">Email : randall.harper@ttu.edu</div>
-  </div>
-</div>
-
-<div class="panel-group">
-  <div class="panel panel-default">
-    <div class="panel-body">James Little</div>
-    <div class="panel-footer">Email : james.little@ttu.edu</div>
-  </div>
-</div>
-
-<div class="panel-group">
-  <div class="panel panel-default">
-    <div class="panel-body">Patrick Braud</div>
-	<div class="panel-footer">Email : patrick.braud@ttu.edu</div>
   </div>
 </div>
 
