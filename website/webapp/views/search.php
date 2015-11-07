2b124b8c14d7322e267c63284c700a24b334347c
James Little <wizkid916@yahoo.com>
2015-11-06 22:49:19 -0600

    Fixed Empty String on Search.

--- website/webapp/views/search.php
+++ website/webapp/views/search.php
@@ -8,38 +8,52 @@ $page->showHeader();
 $page->getModule("categories");
 
 if(isset($_POST["search_submit"])) {
-    $value = $_POST["search"]; 
+    $value = $_POST["search"];
+    unset($_POST);
+    $emptyString = empty($value);
+    $noResults = "No Results Found.";
+    $title = "Showing Results for ".$value.".";
+    if($emptyString) $title = $noResults;
+
+    $page->setTitle($title);
+
     ?>
-    <div class="container col-md-12"> 
+    <div class="container col-md-12">
         <div class="panel-body text-center">
-            <?php echo '<h2>Showing Results for '.$value.'</h2>'; ?>
+            <?php echo "<h2>$title</h2>"; ?>
         </div>
     </div>
     <?php ListCategories($Categories); ?>
     <div class="container col-md-6">
-        <?php
-        foreach($BlogPosts->like($value, "post") as $r) {
+    <?php
+    if (!$emptyString) {
+        $r1 = null;
+        $r2 = null;
+        foreach ($BlogPosts->like($value, "post") as $r) {
             $u = $Users->get($r["user_id"]);
             $cname = $Categories->get($r["category"])["name"];
             $ans = $r["correctAnswerId"] > 0;
             $opened = $r["marked"] == 0;
-            if($opened || $page->isAdmin($Role))
+            if ($opened || $page->isAdmin($Role))
                 blogPostWithCategory($r["id"], $r["title"], $u["first_name"], $u["last_name"], $r["date_posted"], $r["category"], $cname, $ans, $opened);
         }
 
-        foreach($BlogPosts->like($value, "title") as $r) {
+        foreach ($BlogPosts->like($value, "title") as $r) {
             $u = $Users->get($r["user_id"]);
             $cname = $Categories->get($r["category"])["name"];
             $ans = $r["correctAnswerId"] > 0;
             $opened = $r["marked"] == 0;
-            if($opened || $page->isAdmin($Role))
+            if ($opened || $page->isAdmin($Role))
                 blogPostWithCategory($r["id"], $r["title"], $u["first_name"], $u["last_name"], $r["date_posted"], $r["category"], $cname, $ans, $opened);
         }
-
     } else {
-        echo "Nothing to show.";
+        echo $noResults;
     }
-    ?>
+
+} else {
+    echo $noResults;
+}
+?>
     </div>
 
 <?php
