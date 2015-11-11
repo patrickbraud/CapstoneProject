f7d130b5167da361e9ce38685e31abd3b31c8077
James Little <wizkid916@yahoo.com>
2015-11-11 02:46:18 -0600

    fixed profile page junk

--- website/webapp/views/profile.php
+++ website/webapp/views/profile.php
@@ -1,106 +1,99 @@
 <?php
-	ob_start();
-    $page = new Page("Home", $SessionPerson);
-	$page->getModule("categories");
-	$page->getModule("blogPost");
-	$page->getModule("pagination");
+ob_start();
+$page = new Page("Home", $SessionPerson);
+$page->getModule("categories");
+$page->getModule("blogPost");
+$page->getModule("pagination");
 
 
-	$id = $page->getQuery("id");
-	$points = 0;
-	$email = "";
-	$firstName = "";
-	$lastName = "";
+$id = $page->getQuery("id");
+$points = 0;
+$email = "";
+$firstName = "";
+$lastName = "";
 
-	$number = $page->getQuery("number");
-	if (!isset($number)) $number = 1;
+$number = $page->getQuery("number");
+if (!isset($number)) $number = 1;
 
-	$offset = calcOffset($number);
-
-
-	if (!is_null($id)) { //Look up user
-		$u = $Users->get($id);
-		$firstName = $u["first_name"];
-		$lastName = $u["last_name"];
-		$points = $u["points"];
-		$email = $u["email"];
-	} else { //Use Session Person :)
-		$id = $SessionPerson->id();
-		$firstName = $SessionPerson->first_name();
-		$lastName = $SessionPerson->last_name();
-		$points = $SessionPerson->points();
-		$email = $SessionPerson->email();
-	}
-
-	if(isset($_POST["submit"]) && $page->getQuery("sent")) {
-		$fileName = $id.".png";
-		move_uploaded_file($_FILES['avatar']['tmp_name'], ICONS_DIR.$fileName);
-		$page->removeQuery("sent");
-		$page->redirect();
-	} else {
-
-			$page->showHeader();
-		?>
+$offset = calcOffset($number);
 
 
-		<div class="container col-md-8">
-			<div class="panel-body text-center">
-				<?php
-					$ua = new UserAvatar($id);
-<<<<<<< HEAD
-					echo $ua->getImage(120, 120);
-=======
-					echo $ua->getImage(80, 80);
-				
-					if($id == $SessionPerson->id()) {
->>>>>>> origin/master
-				?>
-					<br/>
-					<form action="?page=profile&sent=true"method="post" enctype="multipart/form-data">
-						<input type="file" name="avatar" id="avatar" value="Upload Avatar" />
-						<input type="submit" name="submit" id="submit" value="Upload" />
-					</form>
-				<?php } ?>
+if (!is_null($id)) { //Look up user
+	$u = $Users->get($id);
+	$firstName = $u["first_name"];
+	$lastName = $u["last_name"];
+	$points = $u["points"];
+	$email = $u["email"];
+} else { //Use Session Person :)
+	$id = $SessionPerson->id();
+	$firstName = $SessionPerson->first_name();
+	$lastName = $SessionPerson->last_name();
+	$points = $SessionPerson->points();
+	$email = $SessionPerson->email();
+}
 
-				<h2><?php echo $firstName . " " . $lastName; ?> : <?php echo $points; ?> </h2>
+if(isset($_POST["submit"]) && $page->getQuery("sent")) {
+	$fileName = $id.".png";
+	move_uploaded_file($_FILES['avatar']['tmp_name'], ICONS_DIR.$fileName);
+	$page->removeQuery("sent");
+	$page->redirect();
+} else {
 
-				<h3><?php echo $email; ?></h3>
-			</div>
-		</div>
+	$page->showHeader();
+	?>
 
-		<?php
-		listCategories($Categories);
-		?>
 
-		<div class="container col-md-8">
+	<div class="container col-md-8">
+		<div class="panel-body text-center">
 			<?php
-			$posts = $BlogPosts->getAllForUser($id, ITEMS_PER_PAGE, $offset);
-			$count = count($posts);
-			if ($count > 0) {
-				foreach ($posts as $b) {
-					$categoryName = $Categories->get($b["category"])["name"];
-					$ans = $b["correctAnswerId"] > 0;
-					$opened = $b["marked"] == 0;
-					if ($opened || $page->isAdmin($Role))
-						blogPostWithCategory($b["id"], $b["title"], $id, $firstName, $lastName, $b["date_posted"], $b["category"], $categoryName, $ans, $opened);
-				}
-				pagination($number, $count, $page);
-			} else {
-				?>
-				<div class="panel panel-default">
-					<div class="panel-body text-center">
-						<div class="row">
-							<p>No Posts.</p>
-						</div>
+			$ua = new UserAvatar($id);
+			echo $ua->getImage(120, 120);
+			?>
+			<br/>
+			<form action="?page=profile&sent=true"method="post" enctype="multipart/form-data">
+				<input type="file" name="avatar" id="avatar" value="Upload Avatar" />
+				<input type="submit" name="submit" id="submit" value="Upload" />
+			</form>
+
+			<h2><?php echo $firstName . " " . $lastName; ?> : <?php echo $points; ?> </h2>
+
+			<h3><?php echo $email; ?></h3>
+		</div>
+	</div>
+
+	<?php
+	listCategories($Categories);
+	?>
+
+	<div class="container col-md-8">
+		<?php
+		$posts = $BlogPosts->getAllForUser($id, ITEMS_PER_PAGE, $offset);
+		$count = count($posts);
+		if ($count > 0) {
+			foreach ($posts as $b) {
+				$categoryName = $Categories->get($b["category"])["name"];
+				$ans = $b["correctAnswerId"] > 0;
+				$opened = $b["marked"] == 0;
+				if ($opened || $page->isAdmin($Role))
+					blogPostWithCategory($b["id"], $b["title"], $id, $firstName, $lastName, $b["date_posted"], $b["category"], $categoryName, $ans, $opened);
+			}
+			pagination($number, $count, $page);
+		} else {
+			?>
+			<div class="panel panel-default">
+				<div class="panel-body text-center">
+					<div class="row">
+						<p>No Posts.</p>
 					</div>
 				</div>
-				<?php
+			</div>
+			<?php
 
-			}
-			?>
-		</div>
+		}
+		?>
+	</div>
 
-		<?php
-	}
-    $page->showFooter();
+	<?php
+}
+$page->showFooter();
 ?>
