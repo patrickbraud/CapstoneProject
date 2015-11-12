688efdbd71a99965d92328957723bba3684cb7b6
Patrick <patrick.braud@ttu.edu>
2015-11-12 01:01:21 -0600

    Pushed James' Buttons

--- website/webapp/views/admin/home.php
+++ website/webapp/views/admin/home.php
@@ -11,13 +11,13 @@
 	<div class="form-group">
 		<div class="col-md-4">
 
-			<button type="edit_users" class="btn btn-default" ><a href="<?php echo $page->link("users", "admin"); ?>">Edit Users</a></button>
+			<button type="button" class="btn btn-default" ><a href="<?php echo $page->link("users", "admin"); ?>">Edit Users</a></button>
 		</div>
 	</div>
 
 	<div class="form-group">
 		<div class="col-md-4">
-			<button type="show_categories" class="btn btn-default"><a href="<?php echo $page->link("categories", "admin"); ?>">Categories</button>
+			<button type="button" class="btn btn-default"><a href="<?php echo $page->link("categories", "admin"); ?>">Categories</button>
 		</div>
 	</div>
 <?php /*
